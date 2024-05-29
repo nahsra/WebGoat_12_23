8244e649b27815d49bea9d81ad9a973f4bee9433
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2024-05-29 19:57:07 -0400

    add contact search and logout

---
+++ src/main/org/owasp/webgoat/vulnerable_components/ContactController.java
@@ -0,0 +1,31 @@
+package org.owasp.webgoat.vulnerable_components;
+
+import org.owasp.webgoat.LessonDataSource;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.*;
+
+/** Handle contact management with two new endpoints. */
+@RestController
+public final class ContactController {
+
+    private final LessonDataSource dataSource;
+
+    public ContactController(LessonDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @RequestMapping(path = "/search", method = {RequestMethod.GET, RequestMethod.POST})
+    public @ResponseBody 
+    String search(String q) throws SQLException {
+      // get the phone number from the database
+      return contactDao.search();
+    }
+
+    @RequestMapping("/search")
+    public @ResponseBody
+    void logout() throws SQLException {
+        currentSession().logout();
+    }
+
+}
